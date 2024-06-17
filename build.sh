2f7ce63e23adac00ca6862622b561ee937ab41fe
line <line.xj.lin@gmail.com>
2024-06-18 00:21:02 +0800

    no cgo in darwin

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-go build -o inputGPT -ldflags="-s -w" main.go darwin.go language.go userCore.go config.go utils.go renderMessages.go systray.go hotkey.go gpt.go
+go build -o inputGPT -ldflags="-s -w" main.go darwin.go language.go userCore.go config.go utils.go renderMessages.go systray.go core.go keytap_darwin.go 
